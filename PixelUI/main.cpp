e74467970bd991cbed7716e96ee950d8a6374da0
pixel-Teee <1173769234@qq.com>
2022-10-03 00:03:20 +0800

    add glfw

---
+++ PixelUI/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#include <glfw/glfw3.h>
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "PixelUI", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	return 0;
+}
---
+++ Scripts/Win-GenProjects-2019.bat
@@ -0,0 +1,6 @@
+pushd ..\
+mkdir build
+cd build
+cmake .. -G"Visual Studio 16 2019"
+popd
+PAUSE

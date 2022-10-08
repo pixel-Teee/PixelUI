fb88f2edeaf896c276fb119dd7b7ea6955be3f3f
pixel-Teee <1173769234@qq.com>
2022-10-08 18:39:27 +0800

    render window

--- PixelUI/main.cpp
+++ PixelUI/main.cpp
@@ -1,40 +1,82 @@
-#include <iostream>
+#include <bgfx/bgfx.h> //include bgfx
 
-#include <bgfx/bgfx.h>
+#include <bgfx/platform.h>
 
-#include <glfw/glfw3.h>
-#if BX_PLATFORM_LINUX
-#define GLFW_EXPOSE_NATIVE_X11
-#elif BX_PLATFORM_WINDOWS
+#include <bx/math.h>
+
+#include <GLFW/glfw3.h>
+
 #define GLFW_EXPOSE_NATIVE_WIN32
-#elif BX_PLATFORM_OSX
-#define GLFW_EXPOSE_NATIVE_COCOA
-#endif
 #include <GLFW/glfw3native.h>
+
+#define WIDTH 1600
+#define HEIGHT 800
+
+#include <iostream>
 
 int main()
 {
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	GLFWwindow* window = glfwCreateWindow(800, 600, "PixelUI", NULL, NULL);
-	if (window == NULL)
+	//glfw init
+	if (!glfwInit())
 	{
-		std::cout << "Failed to create GLFW window" << std::endl;
+		std::cout << "create glfw error!" << std::endl;
+	}
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+	//create window
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "hello bgfx!", NULL, NULL);
+
+	if (!window)
+	{
+		std::cout << "create glfw window error!" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
+
+	//make the window context current
 	glfwMakeContextCurrent(window);
+
+	//------init bgfx------
+	bgfx::PlatformData pd;
+	pd.ndt = NULL;
+	pd.nwh = glfwGetWin32Window(window);
+
+	bgfx::Init bgfxInit;
+	bgfxInit.type = bgfx::RendererType::Count;
+	bgfxInit.resolution.width = WIDTH;
+	bgfxInit.resolution.height = HEIGHT;
+	bgfxInit.resolution.reset = BGFX_RESET_VSYNC;
+	bgfxInit.platformData = pd;
+
+	if (!bgfx::init(bgfxInit))
+	{
+		std::cout << "create bgfx error!" << std::endl;
+		return -1;
+	}
+	//------init bgfx------
+
+	bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x303030ff, 1.0f, 0);
+	bgfx::setViewRect(0, 0, 0, WIDTH, HEIGHT);
 
 	while (!glfwWindowShouldClose(window))
 	{
+		//render here
+		bgfx::touch(0);
+		//glClear(GL_COLOR_BUFFER_BIT);
+		bgfx::frame();
+
+		//swap front and back buffers
 		glfwSwapBuffers(window);
+
+		//poll for and process event
 		glfwPollEvents();
 	}
 
-	bgfx::init();
+	//shut down bgfx
+	bgfx::shutdown();
+
+	glfwTerminate();
 
 	return 0;
 }
